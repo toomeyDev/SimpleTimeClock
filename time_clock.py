70e4b6b47e73cf50d68ea61b4969dcfd4aef7a35
Matt <mtoomeydev@gmail.com>
2021-08-18 13:42:20 -0400

    refactor most code to use functional logic

--- time_clock.py
+++ time_clock.py
@@ -20,133 +20,206 @@ def load_dataset(data_path: str) :
 
 data = [load_dataset('commands.json')] #hold different json files for access during runtime
 
-
+# menu functionality (intro, set filename, display help, commands
 def intro():
-	"""Introduce the user to program functionality, entry format."""
-	global runtime, command_phrases, output_file
-	print("Welcome to Timeclock, a simple terminal application.")
-	print("You can add entries in hours, minutes, and seconds.")
-	print("When you are finished with entry, type 'submit'.\n")
-	print("To see available entry formats, type 'formats-help'.\n")
-	print("To set filename for saving timesheet, type 'filename-set'.\n")
-	print("For a full list of available commands, type 'commands'\n")
-	ans = input("Hit enter to continue, or type a command.\n")
-	while ans != '':
-		if ans.lower() == 'filename-set':
-			filename=input("Enter name for txt output of timesheet: ")
-			print(f"Entries will be written to {filename}.\n\n")
-			output_file = filename
-		elif ans.lower() == 'formats-help':
-			with open('input_formats.txt') as file:
-				print(file.read())
-		elif ans.lower() == 'commands':
-				for key, value in data[0].items():
-					if(key == "Available Commands"):
-						print(f"{key}\n")
-						continue
-					print(f"-> {key} | {value}\n")
-		elif ans.lower() == 'clear':
-			clear_screen()
-		elif ans.lower() == 'exit':
-			clear_screen()
-			exit("Exiting...")
-		else: 
-			print(f"Unrecognized command: {ans}\n")
-		ans = input("\nHit enter to continue, or type a command.\n")
-	runtime = True # set value to true to execute main loop
-	
-def entry_prompt():
-	global time_sheet, runtime, total, output_file, entry_mode
-	if entry_mode == 1:
-		filename = input("Enter filename for new timesheet:\n"
-		+ "[leave blank to use the same filename again]\n")
-		if filename == "":
-			filename = output_file
-		print(f"Entries will be written to {filename}.\n\n")
-		output_file = filename
-		entry_mode = 0
-	while True:
-		hrs = input("Hours: ")
-		mts = input("Minutes: ")
-		sec = input("Sec: ")
-		print(f"{hrs} hours, {mts} minutes, and {sec} seconds, is this correct?")
-		ans = input("Enter (y/n), type 'submit' to finish entry: ")
-	
-		# prompt for confirmation
-		if ans.lower() == 'y':
-			# add entered values to total
-			total[0] += int(hrs)
-			total[1] += int(mts)
-			total[2] += int(sec)
-		
-			time_sheet += f"-> {hrs} hours, {mts} minutes, {sec} seconds\n"
-			clear_screen()
-		elif ans.lower() == 'submit':
-			print("Submitting...")
-			# add entered values to total
-			total[0] += int(hrs)
-			total[1] += int(mts)
-			total[2] += int(sec)
-			format_time()
-			time_sheet += f"-> {hrs} hours, {mts} minutes, {sec} seconds\n"
-			time_sheet += f"|# Total: {total[0]} hours, {total[1]} minutes, {total[2]} seconds #|"
-			sleep(1)
-			clear_screen()
-			break
+    """Introduce the user to program functionality, entry format."""
+    print("Welcome to Timeclock, a simple terminal application.")
+    print("You can add entries in hours, minutes, and seconds.")
+    print("When you are finished with entry, type 'submit'.\n")
+    print("To see available entry formats, type 'formats-help'.\n")
+    print("To set filename for saving timesheet, type 'filename-set'.\n")
+    print("For a full list of available commands, type 'commands'\n")
+
+
+def set_filename():
+    global output_file
+    """Set the filename where finished timesheet results will be saved."""
+    filename=input("Enter name for text output of new timesheet: \n"
+    +f"[Leave empty to reuse the current filename {output_file}]")
+    if filename == '':
+        filename = output_file
+        
+    print(f"Entries will be written to {filename}.\n\n")
+    output_file = filename
+
+
+def formats_help():
+    """
+    Display information about available formats and current format
+    settings for time entry.
+    """
+    with open('input_formats.txt') as file:
+        print(file.read())
+        
+
+def commands_list():
+    """
+    Display a list of available commands to the user from the
+    'commands.json' file.
+    """
+    for key, value in data[0].items():
+        if(key == "Available Commands"):
+            print(f"{key}\n")
+            continue
+        print(f"-> {key} | {value}\n")
+        
+
+def exit_program():
+    """Terminate the program."""
+    clear_screen()
+    print("Exiting...")
+    sleep(1)
+    exit()
+
+        
+def menu_sequence():
+    """Handle user input commands and control-flow of initial menu."""
+    global entry_mode
+    if(entry_mode == 0):
+        ans = input("Hit enter to continue, or type a command.\n")
+    else:
+        ans = input("Hit enter to start a timesheet, or type a command.\n")
+    while ans != '':
+        if ans.lower() == 'filename-set':
+            set_filename()
+        elif ans.lower() == 'formats-help':
+            formats_help()
+        elif ans.lower() == 'commands':
+            commands_list()
+        elif ans.lower() == 'clear':
+            clear_screen()
+        elif ans.lower() == 'exit':
+            exit_program()
+        else: 
+            print(f"Unrecognized command: {ans}\n")
+        ans = input("\nHit enter to continue, or type a command.\n")
+        
+
+def log_time():
+    hrs = int(input("Hours: "))
+    mts = int(input("Minutes: "))
+    sec = int(input("Sec: "))
+    print(f"{hrs} hours, {mts} minutes, and {sec} seconds, correct?")
+    ans = input("Enter (y/n), type 'submit' to finish entry: ")
+    output = [hrs, mts, sec, ans] # store time, response to prompt
+    return output
+
+
+def add_to_total(entry: []):
+    global total, time_sheet
+    # add entered values to total
+    total[0] += entry[0]
+    total[1] += entry[1]
+    total[2] += entry[2]
+    time_sheet += (f"-> {entry[0]} hours, {entry[1]} minutes," 
+    + f" {entry[2]} seconds\n")
+    
+    # clear screen, print out current timesheet for user
+    clear_screen()
+    print(time_sheet)
+
 
 def format_time():
-	"""
-	Format the current total values, converting values
-	greater than or equal to 60 to their equivalent larger
-	time denominations (ie: 120 seconds becomes 2 minutes,
-	180 minutes becomes three hours and so on).
-	"""
-	index = 0
-	for item in total:
-		if item / 60 >= 1 and index != 0:
-			item_remainder = item % 60
-			quotient = int(item / 60)
-			total[index - 1] += quotient
-			total[index] = item_remainder
-		index += 1
+    """
+    Format the current total values, converting values
+    greater than or equal to 60 to their equivalent larger
+    time denominations (ie: 120 seconds becomes 2 minutes,
+    180 minutes becomes three hours and so on).
+    """
+    index = 0
+    for item in total:
+        if item / 60 >= 1 and index != 0:
+            item_remainder = item % 60
+            quotient = int(item / 60)
+            total[index - 1] += quotient
+            total[index] = item_remainder
+        index += 1
 
-			
+
+def submit_time(entry: []):
+    """
+    Format and submit current accumulated totals to the
+    timesheet string before they are written to output.
+    """
+    global total, time_sheet
+    print("Submitting...")
+    sleep(1)
+    
+    add_to_total(entry)
+    format_time()
+    
+    time_sheet += (f"|# Total: {total[0]} hours, {total[1]} minutes," 
+    +f" {total[2]} seconds #|")
+    
+    clear_screen()
+    
+def entry_sequence():
+    global time_sheet, runtime, total, output_file, entry_mode
+    if entry_mode == 1:
+        set_filename()
+        entry_mode = 0
+    while True:
+        entry = log_time() #entry[0]=hrs, entry[1]=mts, entry[2]=sec
+        ans = entry[3] # store user response to validation prompt
+        if ans == 'y':
+            add_to_total(entry)
+        elif ans == 'submit':
+            submit_time(entry)
+            break
+        else:
+            print("Discarding entry...")
+            sleep(1)
+            clear_screen()
+
+
+def reset_values():
+    """Reset values necessary for running the program to initial state."""
+    global total, timesheet
+    index = 0
+    for value in total:
+        total[index] = 0
+        index += 1
+    timesheet = "Entries: \n"
+
+            
 def save_entries():
-	global time_sheet, output_file, runtime, entry_mode
-	if path.isfile(output_file):
-	    ans=input(f"Are you sure you want to overwrite the file {output_file}?\n")
-	    if ans.lower() == 'n' or ans.lower() == 'no':
-	        output_file = input("Choose an alternate filename for output: ")
-	    else:
-	        print(f"{output_file} will be overwritten.")
-	        
-	with open(output_file, 'w') as file:
-	    file.write(time_sheet)
-	
-	print(f"Entries saved to '{output_file}'\n")
-	ans = input("Fillout another timesheet? (y/n)\n")
-	if ans.lower() != 'y':
-		runtime = False
-		print("Exiting...")
-	else:
-		entry_mode = 1
-
-
+    global output_file, runtime, entry_mode
+    if path.isfile(output_file):
+        ans=input(f"Are you sure you want to overwrite the file {output_file}?\n")
+        if ans.lower() == 'n' or ans.lower() == 'no':
+            output_file = input("Choose an alternate filename for output: ")
+        else:
+            print(f"{output_file} will be overwritten.")
+    # write recorded time to file using specified name
+    with open(output_file, 'w') as file:
+        file.write(time_sheet)
+        print(f"Entries saved to '{output_file}'\n")
+    
+    ans = input("Fill out another timesheet? (y/n)\n")
+    if ans.lower() != 'y':
+        exit_program()
+    else:
+        entry_mode = 1
+        reset_values()
+        clear_screen()
+        
+        
 def clear_screen():
-	"""Clear the terminal/console of any generated text."""
-	# make use of 'cls' command to clear screen in Windows
-	if name == 'nt':
-		system('cls')
-	# use 'clear' for all other operating systems (linux, macosx etc)
-	else:
-		system('clear')
-
+    """Clear the terminal/console of any generated text."""
+    # make use of 'cls' command to clear screen in Windows
+    if name == 'nt':
+        system('cls')
+    # use 'clear' for all other operating systems (linux, macosx etc)
+    else:
+        system('clear')
+        
 def main():
-	intro()
-	while runtime:
-		entry_prompt()
-		print(time_sheet)
-		save_entries()	
+    intro()
+    while True:
+        menu_sequence()
+        entry_sequence()
+        save_entries()
 
-if __name__ == '__main__':	
-	main()
+if __name__ == '__main__':
+    main()
+    
