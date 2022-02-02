a291301e7a91daeb7061ec0f107267fc78c0528b
Matthew Toomey <mtoomeydev@gmail.com>
2022-02-01 20:53:28 -0500

    add some basic GUI elements to the frontend

--- frontend.py
+++ frontend.py
@@ -1,5 +1,14 @@
 import tkinter as tk
 
 window = tk.Tk()
-greeting = tk.Label(text="Hello")
-greeting.pack()
+window.rowconfigure([0, 1, 2], minsize=50)
+window.columnconfigure([0,1,2], minsize= 150)
+
+frm_header = tk.Frame(master=window, borderwidth=2)
+frm_header.grid(row=0, column=1)
+
+lbl_title = tk.Label(master=frm_header, text="SimpleTimeClock")
+lbl_title.pack()
+
+# Run the event loop
+window.mainloop()
